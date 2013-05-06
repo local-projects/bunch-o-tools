0152cbe71a3bd686094910366053718b281a456b
David Scharf <dscharf@gmx.net>
2013-05-06 10:59:47 +0200

    added comments to color factories

--- src/UIColor+BOTFactories/UIColor+BOTFactories.h
+++ src/UIColor+BOTFactories/UIColor+BOTFactories.h
@@ -4,7 +4,10 @@
 
 @interface UIColor (BOTFactories)
 
+//create ui color from a string such as @"#FF00FF"
 + (UIColor *) colorFromRGBString:(NSString *) s;
+
+//create ui color from a string such as @"#77FF00FF"
 + (UIColor *) colorFromARGBString:(NSString *) s;
 
 @end
